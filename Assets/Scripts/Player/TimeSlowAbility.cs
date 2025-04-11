b736bec42f066e9a25e274b6c152ae4e9ad67394
ArnoldasSkrodenis <arnskr@ktu.lt>
2025-04-11 23:15:49 +0300

    Time slowdown ability
    Added an ability that lets you slowdown time for 2 seconds. Currently is activated by the e key and has a 5s cooldown.

---
+++ Assets/Scripts/Player/TimeSlowAbility.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+
+public class TimeSlowAbility : MonoBehaviour
+{
+    [Header("Time Slow Settings")]
+    public float slowFactor = 0.1f;// 0 = frozen, 1 = normal speed
+    public float slowDuration = 2f;
+    public float cooldownTime = 5f;
+    private float nextUseTime = 0f;
+    public KeyCode abilityKey = KeyCode.E;
+
+    private bool isSlowingTime = false;
+
+    private PlayerMovement playerMovement;
+    private float originalSpeedMultiplier;
+
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(abilityKey) && Time.time >= nextUseTime && !isSlowingTime)
+        {
+            nextUseTime = Time.time + cooldownTime;
+            StartCoroutine(SlowTime());
+        }
+    }
+
+    IEnumerator SlowTime()
+    {
+        isSlowingTime = true;
+
+        // Slow down time
+        Time.timeScale = slowFactor;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        originalSpeedMultiplier = playerMovement.GetCurrentSpeedMultiplier();
+
+        playerMovement.ModifySpeedMultiplier(originalSpeedMultiplier / slowFactor);
+        Debug.Log("Slow");
+        yield return new WaitForSecondsRealtime(slowDuration);
+
+        // Restore time
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
+        playerMovement.ModifySpeedMultiplier(originalSpeedMultiplier);
+
+        isSlowingTime = false;
+    }
+}
